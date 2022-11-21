bbff3256d8dd26b1b99a1e196b6e8679724eb01c
derpyzza <derpyzza@gmail.com>
2022-11-21 19:23:16 +0500

    Fetches Latest jam from the official mini jam api
    changes: simply fetches the latest jam and prints it out to the terminal
    as a json string. next step would be to change the latest jam fetching
    functionality to it's function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,28 +11,53 @@ import (
 )
 
 type Jam struct {
-	Name   string `json:"name"`
-	Number string `json:"number"`
-	URL    string `json:"url"`
-	Image  string `json:"image"`
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Image string `json:"image"`
 }
 
+// return array of jams of requested size
 func getJams(w http.ResponseWriter, r *http.Request) {
 	jam := Jam{
-		Name:   "Noir",
-		Number: "115",
-		URL:    "https://itch.io/jam/mini-jam-115-noir",
-		Image:  "https://minijamofficial.com/images/MJ115.gif"}
+		ID:    "115",
+		Name:  "Noir",
+		URL:   "https://itch.io/jam/mini-jam-115-noir",
+		Image: "https://minijamofficial.com/images/MJ115.gif"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(jam)
+}
+
+// fetch and return the latest mini jam entry.
+func getLatest(w http.ResponseWriter, r *http.Request) {
+	// todo
 }
 
 func main() {
 	fmt.Println("hello there;")
 
+	// TEMPORARY URL TO FETCH LATEST JAM
+	url := "https://minijamofficial.com/api/fetchMiniJams?n=0"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sb := string(body)
+	fmt.Println(sb)
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/jam", getJams).Methods("GET")
+	// get list of jams
+	router.HandleFunc("/api/jam", getJams).Methods("GET")
+	// get latest jam
+	router.HandleFunc("/api/latest", getLatest).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
